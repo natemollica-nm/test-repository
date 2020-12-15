3cc4175d41128dc00f2ded543e7bba130a32dfff
Nate Mollica <nathanmollica@gmail.com>
2020-12-14 16:48:44 -0800

    Redacting

--- example.rb
+++ example.rb
@@ -1,9 +1,17 @@
 # Launch School Intro to Ruby Project #
-# arrays #
-nums=*(1..100).to_a
+# Redacting #
 
-i = 0
-nums.each do |i|
-  next if i > 50
-  puts "#{i}"
+puts "Text to search through: "
+text = gets.chomp
+puts "Word to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
 end
