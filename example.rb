5184dbc0a1c2672bb9d3d86c373a20c034867e6d
Nate Mollica <nathanmollica@gmail.com>
2020-12-17 19:45:34 -0800

    GitHub Movies Push

--- example.rb
+++ example.rb
@@ -1,17 +1,20 @@
 # Launch School Intro to Ruby Project #
-# Redacting #
+# Movie Ratings #
+movie_ratings = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 5,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+# Add your code below!
 
-puts "Text to search through: "
-text = gets.chomp
-puts "Word to redact: "
-redact = gets.chomp
-
-words = text.split(" ")
-
-words.each do |word|
-  if word != redact
-    print word + " "
-  else
-    print "REDACTED "
+good_movies = movie_ratings.select do |movie, rating|
+  if rating >= 3
+    puts movie
   end
 end
