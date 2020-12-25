1052a1c825effdf21583ca0a4ee5b1f59fccf58b
Nate Mollica <nathanmollica@gmail.com>
2020-12-24 19:51:21 -0800

    some changes to case.rb

--- CodeAcademyPrep/case.rb
+++ CodeAcademyPrep/case.rb
@@ -1,3 +1,14 @@
 # CodeAcademy case statements excercise #
 #########################################
+def double(num)
+  
+  yield(num)
+
+  puts num
+
+end
+
+double(15) { |num| puts num = num * 2 }
+
+
 
